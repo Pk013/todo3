55b4478a56a53dc541e1058a3d594efe95d234e5
morthos <pathshan61@gmail.com>
2018-11-27 13:33:25 -0500

    Updates

---
+++ precache-manifest.3c4e85da8b8cbfc29fde537e6c84c933.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "12e4067199033db8c760",
+    "url": "/todo3/./static/js/runtime~main.12e40671.js"
+  },
+  {
+    "revision": "95e20920dce6dc6bb8e2",
+    "url": "/todo3/./static/js/main.95e20920.chunk.js"
+  },
+  {
+    "revision": "da6842c081a834862d6d",
+    "url": "/todo3/./static/js/1.da6842c0.chunk.js"
+  },
+  {
+    "revision": "95e20920dce6dc6bb8e2",
+    "url": "/todo3/./static/css/main.bc9b6d8f.chunk.css"
+  },
+  {
+    "revision": "ea45d5ca2ab7dba04a78b211db7704df",
+    "url": "/todo3/./index.html"
+  }
+];
